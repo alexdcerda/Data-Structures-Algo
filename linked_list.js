2012598359b9e04b2a3d6108e764a89c736197ab
Alex Cerda <alexdcerda@outlook.com>
2025-02-25 21:05:23 -0500

    linked list JS

--- linked_list.js
+++ linked_list.js
@@ -1,7 +1,33 @@
+class LinkedList {
+  constructor(head = null) {
+    this.head = head;
+  }
 
-
-class LinkedLIist {
-    constructor() {
-        
+  append(value) {
+    if (!this.head) {
+      // check if node is empty
+      this.head = new node(value);
+      return this;
     }
+    let tail = this.getTail();
+    tail.next = new node(value);
+    return tail;
+  }
+  prepend(value) {
+    if (!this.head) {
+      // check if node is empty
+      this.head = new node(value);
+      return this;
+    }
+    const prevHead = this.head;
+    this.head = new node(value, prevHead);
+  }
+  getTail() {
+    if (!this.head) return null; // check if node is empty
+    let tail = this.head;
+    while (tail.next !== null) {
+      tail = tail.next;
+    }
+    return tail;
+  }
 }

0cb7eb19f2329042f31d3779e2abe6eb1480d825
Alex Cerda <alexdcerda@outlook.com>
2025-03-01 10:29:35 -0500

    avoid collision hash

--- hashMaps/testing_hash.js
+++ hashMaps/testing_hash.js
@@ -10,18 +10,37 @@ function hash(name, middleName, surname) {
 console.log(hash(name, middleName, surname))
 
 
+
+
 ///// turn string to number 
-const newName = 'iamcracked'
+const newName = 'Iamcracked'
 
 function stringToNumber(string) {
     let hashCode = 0
     for (let stringLetter = 0; stringLetter < string.length; stringLetter++) {
         hashCode += string.charCodeAt(stringLetter)
     }
-    return hashCode
-        
+    return hashCode      
 }
 function newHash(newName) {
     return stringToNumber(newName)
 }
 console.log(newHash(newName))
+
+
+
+// avoid collisions when hashing: iamcracked and amicracked
+// will now have different hashes
+const newName2 = 'amIcracked'
+
+function stringToNumber2(string) {
+    let hashCode = 0
+    primeNumber = 2
+    for (let stringLetter = 0; stringLetter < string.length; stringLetter++) {
+        hashCode = primeNumber * hashCode + string.charCodeAt(stringLetter)
+    }
+    return hashCode
+}
+console.log("newName2:", stringToNumber2(newName2));
+console.log("newName:", stringToNumber2(newName));
+

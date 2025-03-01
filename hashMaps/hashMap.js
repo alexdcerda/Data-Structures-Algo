1de0216c94d922aff4d7ad742d68764f27c915fd
Alex Cerda <alexdcerda@outlook.com>
2025-03-01 14:55:35 -0500

    hashmap starting point

--- hashMaps/hashMap.js
+++ hashMaps/hashMap.js
@@ -4,3 +4,12 @@ const load_factor = 0.75
 
 
 
+function hash(key) {
+    let hashCode = 0
+
+    const primeNumber = 31
+    for (let i = 0; i < key.length; i++) {
+        hashCode = primeNumber * hashCode + key.charCodeAt(i)
+    }
+    return hashCode
+}

1f451d7e0e679abf08e1011390b52c0a23e590f8
Alex Cerda <alexdcerda@outlook.com>
2025-03-01 00:01:07 -0500

    Moved linked_list.js into linkedLists directory

--- hashMaps/testing_hash.js
+++ hashMaps/testing_hash.js
@@ -1,10 +1,27 @@
 
 
 const name = 'alex'
+const middleName = 'DDDDDDD'
+const surname = 'CCCCCC'
 
-function hash(name) {
-    return name.charAt(0)
+function hash(name, middleName, surname) {
+    return name.charAt(0) + middleName.charAt(0) + surname.charAt(0)
 }
-console.log(hash(name))
+console.log(hash(name, middleName, surname))
 
 
+///// turn string to number 
+const newName = 'iamcracked'
+
+function stringToNumber(string) {
+    let hashCode = 0
+    for (let stringLetter = 0; stringLetter < string.length; stringLetter++) {
+        hashCode += string.charCodeAt(stringLetter)
+    }
+    return hashCode
+        
+}
+function newHash(newName) {
+    return stringToNumber(newName)
+}
+console.log(newHash(newName))
